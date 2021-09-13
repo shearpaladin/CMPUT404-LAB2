384b549a773118b9a315e5de980d7612e0675ea2
shearpaladin <sandy6@ualberta.ca>
2021-09-13 17:09:04 -0600

    adding python files

---
+++ echo_server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import socket
+import time
+
+#define address & buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    
+        #QUESTION 3
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+        #bind socket to address
+        s.bind((HOST, PORT))
+        #set to listening mode
+        s.listen(2)
+        
+        #continuously listen for connections
+        while True:
+            conn, addr = s.accept()
+            print("Connected by", addr)
+            
+            #recieve data, wait a bit, then send it back
+            full_data = conn.recv(BUFFER_SIZE)
+            time.sleep(0.5)
+            conn.sendall(full_data)
+            conn.close()
+
+if __name__ == "__main__":
+    main()
