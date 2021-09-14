869093984856eed4cb878be4386de433eb66c0e3
shearpaladin <sandy6@ualberta.ca>
2021-09-13 19:32:50 -0600

    done to Q2

--- echo_server.py
+++ echo_server.py
@@ -9,24 +9,25 @@ BUFFER_SIZE = 1024
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
+
         #QUESTION 3
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
+
         #bind socket to address
         s.bind((HOST, PORT))
         #set to listening mode
         s.listen(2)
-        
+
         #continuously listen for connections
         while True:
             conn, addr = s.accept()
             print("Connected by", addr)
-            
+
             #recieve data, wait a bit, then send it back
             full_data = conn.recv(BUFFER_SIZE)
             time.sleep(0.5)
             conn.sendall(full_data)
+            print(str(full_data))
             conn.close()
 
 if __name__ == "__main__":
