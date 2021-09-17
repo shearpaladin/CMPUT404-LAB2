c18b8c0d0c5d07cebda3fb2fb5b472073641a00a
shearpaladin <sandy6@ualberta.ca>
2021-09-16 21:10:31 -0600

    Almost done on step 8, just have to make multi-proxies

--- proxy_client.py
+++ proxy_client.py
@@ -6,71 +6,30 @@ HOST = "localhost"
 PORT = 8001
 BUFFER_SIZE = 1024
 
-#create a tcp socket
-def create_tcp_socket():
-    print('Creating socket')
+# Step 7
+address = ('127.0.0.1', 8001)
+
+payload = "GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n"
+
+def connect(addr):
+    #create socket, connect, and recieve data
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except (socket.error, msg):
-        print(f'Failed to create socket. Error code: {str(msg[0])} , Error message : {msg[1]}')
-        sys.exit()
-    print('Socket created successfully')
-    return s
+        s.connect(addr)
+        s.sendall(payload.encode())
+        s.shutdown(socket.SHUT_WR) #shutdown() is different from close()
 
-#get host information
-def get_remote_ip(host):
-    print(f'Getting IP for {host}')
-    try:
-        remote_ip = socket.gethostbyname( host )
-    except socket.gaierror:
-        print ('Hostname could not be resolved. Exiting')
-        sys.exit()
-
-    print (f'Ip address of {host} is {remote_ip}')
-    return remote_ip
-
-#send data to server
-def send_data(serversocket, payload):
-    print("Sending payload")
-    try:
-        serversocket.sendall(payload.encode())
-    except socket.error:
-        print ('Send failed')
-        sys.exit()
-    print("Payload sent successfully")
-
-def main():
-    try:
-        #define address info, payload, and buffer size
-        host = 'www.google.com'
-        port = 80
-        payload = f'GET / HTTP/1.0\r\nHost: {host}\r\n\r\n'
-        buffer_size = 4096
-
-        #make the socket, get the ip, and connect
-        s = create_tcp_socket()
-
-        remote_ip = get_remote_ip(host)
-
-        s.connect((remote_ip , port))
-        print (f'Socket Connected to {host} on ip {remote_ip}')
-
-        #send the data and shutdown
-        send_data(s, payload)
-        s.shutdown(socket.SHUT_WR)
-
-        #continue accepting data until no more left
-        full_data = b""
-        while True:
-            data = s.recv(buffer_size)
-            if not data:
-                 break
-            full_data += data
+        full_data = s.recv(BUFFER_SIZE)
         print(full_data)
+
     except Exception as e:
         print(e)
     finally:
-        #always close at the end!
+        #remember to close connection!
         s.close()
+
+def main():
+    connect(address)
+
 if __name__ == "__main__":
     main()
--- proxy_server.py
+++ proxy_server.py
@@ -7,28 +7,60 @@ HOST = ""
 PORT = 8001
 BUFFER_SIZE = 1024
 
+#get host information
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print ('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print (f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
-        #QUESTION 3
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    extern_host = "www.google.com"
+    port = 80
 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_start:
+        print("Starting proxy server")
+        # allow reused addresses, bind, and set to listening mode
+        proxy_start.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         #bind socket to address
-        s.bind((HOST, PORT))
+        proxy_start.bind((HOST, PORT))
         #set to listening mode
-        s.listen(2)
+        proxy_start.listen(1)
 
         #continuously listen for connections
         while True:
-            conn, addr = s.accept()
+            # connect to proxy_start
+            conn, addr = proxy_start.accept()
             print("Connected by", addr)
 
-            #recieve data, wait a bit, then send it back
-            full_data = conn.recv(BUFFER_SIZE)
-            time.sleep(0.5)
-            conn.sendall(full_data)
-            print(full_data)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end:
+                print("Connecting to Google")
+                remote_ip = get_remote_ip(extern_host)
+
+                #connect to proxy_end
+                proxy_end.connect((remote_ip, port))
+
+                #send data
+                send_full_data = conn.recv(BUFFER_SIZE)
+                print(f"Sending recieved data {send_full_data} to google")
+                proxy_end.sendall(send_full_data)
+
+                # remember to shut down!!
+                proxy_end.shutdown(socket.SHUT_WR) #shutdown() is different from close()
+
+                data = proxy_end.recv(BUFFER_SIZE)
+                print(f"Sending recieved data {data} to client")
+                #send data back
+                conn.send(data)
+
             conn.close()
+                
 
 if __name__ == "__main__":
     main()
